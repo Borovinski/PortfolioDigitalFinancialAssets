9af54d20a54abddfc46efc883a5ae422caaba16e
helli <hellinxskyline@gmail.com>
2024-10-20 18:01:15 +0300

    Initial commit

---
+++ src/main/java/com/borovinski/portfoliodigitalfinancialassets/PortfolioDigitalFinancialAssetsApplication.java
@@ -0,0 +1,13 @@
+package com.borovinski.portfoliodigitalfinancialassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioDigitalFinancialAssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioDigitalFinancialAssetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/borovinski/portfoliodigitalfinancialassets/PortfolioDigitalFinancialAssetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borovinski.portfoliodigitalfinancialassets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioDigitalFinancialAssetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
